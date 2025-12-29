f5e5d1b857203fa8307b61a95aa1504d53425963
Sundararajan S <110343412+Sundar-1002@users.noreply.github.com>
2025-12-29 21:34:49 +0000

    Trained a new model with different layer and kernels.

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+#Importing Libratries
 import tensorflow as tf
 import keras
 from keras.preprocessing.image import img_to_array, load_img 
@@ -11,23 +12,34 @@ from tensorflow.keras.layers import Conv3D, ConvLSTM2D, Conv3DTranspose
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 import imutils
 
+# Load the preprocessed training data
 training_data = np.load('./Avenue Dataset/training_data.npy')
-frames = training_data.shape[2]
-frames = frames - frames%10
+# Ensure the number of frames is divisible by 10
+frames = training_data.shape[2] # Get the number of frames
+frames = frames - frames%10 # Adjust frames to be divisible by 10
 
-training_data = training_data[:,:,:frames]
+training_data = training_data[:,:,:frames]# Trim the data to the adjusted number of frames
+# Reshape the data to have sequences of 10 frames each
 training_data = training_data.reshape(-1, 227, 227, 10)
+# Expand dimensions to add channel dimension
 training_data = np.expand_dims(training_data, axis=4)
-target_data = training_data.copy()
+target_data = training_data.copy() # For autoencoder, target is same as input
 
 epochs = 5
 batch_size = 16
 
+# Building the model
+# The model is a combination of Conv3D and ConvLSTM2D layers to capture spatiotemporal features from video frames.
+#The first 3 layers are Conv3D layers which extract spatial features from the input frames.
+# The next 3 layers are ConvLSTM2D layers which capture temporal dependencies across the frames.
+# The last 3 layers are Conv3DTranspose layers which reconstruct the output frames from the learned features.
 model=Sequential()
 
-model.add(Conv3D(filters=128,kernel_size=(11,11,1),strides=(4,4,1),padding='valid',input_shape=(227,227,10,1),activation='tanh'))
+model.add(Conv3D(filters=32,kernel_size=(5,5,1),strides=(2,2,1),padding='valid',input_shape=(227,227,10,1),activation='tanh'))
 
-model.add(Conv3D(filters=64,kernel_size=(5,5,1),strides=(2,2,1),padding='valid',activation='tanh'))
+model.add(Conv3D(filters=64,kernel_size=(3,3,1),strides=(2,2,1),padding='valid',activation='tanh'))
+
+model.add(Conv3D(filters=128,kernel_size=(3,3,1),strides=(2,2,1),padding='valid',activation='tanh'))
 
 model.add(ConvLSTM2D(filters=64,kernel_size=(3,3),strides=1,padding='same',dropout=0.4,recurrent_dropout=0.3,return_sequences=True))
 
@@ -35,13 +47,17 @@ model.add(ConvLSTM2D(filters=32,kernel_size=(3,3),strides=1,padding='same',dropo
 
 model.add(ConvLSTM2D(filters=64,kernel_size=(3,3),strides=1,return_sequences=True, padding='same',dropout=0.5))
 
-model.add(Conv3DTranspose(filters=128,kernel_size=(5,5,1),strides=(2,2,1),padding='valid',activation='tanh'))
+model.add(Conv3DTranspose(filters=128,kernel_size=(3,3,1),strides=(2,2,1),padding='valid',activation='tanh'))
 
-model.add(Conv3DTranspose(filters=1,kernel_size=(11,11,1),strides=(4,4,1),padding='valid',activation='tanh'))
+model.add(Conv3DTranspose(filters=64,kernel_size=(4,4,1),strides=(2,2,1),padding='valid',activation='tanh'))
 
+model.add(Conv3DTranspose(filters=1,kernel_size=(5,5,1),strides=(2,2,1),padding='valid',activation='tanh'))
+
+# Compiling the model
 model.compile(optimizer='adam',loss='mean_squared_error', metrics=['accuracy'])
 
+# Training the model
 model.fit(training_data,target_data, batch_size=batch_size, epochs=epochs)
 
 # Saving the model
-model.save("./model/saved_model.keras")
+model.save("./model/saved_model-ExtraLayers.keras")
--- video2array.py
+++ video2array.py
@@ -1,48 +1,59 @@
+#Importing Libratries
 import numpy as np
 import cv2 as cv
 import os   
 from keras.preprocessing.image import img_to_array, load_img
 
+# This img_data list will hold all the processed images. These images are nothing `but frames extracted from all the training videos. 
 img_data = []
 train_path = './Avenue Dataset/training_videos/'
+#Get list of all training videos.
 train_videos = os.listdir(train_path)
+#This is a folder to temporarily store all the extracted frames from training videos. For each video, the frames stored in this folder will be overwritten.
 train_img_path = './Avenue Dataset/training-frames/'
 
+#If the path does not exist, create it.
 if not os.path.exists(train_img_path):
     os.makedirs(train_img_path)
 
+# This function is used load the image from the given path, resize it to 227x227, convert it to grayscale and append it to the img_data list.
 def data_store(image_path):
-    img = load_img(image_path)
-    img = img_to_array(img)
-    img = cv.resize(img, (227,227), interpolation=cv.INTER_CUBIC)
-    gray =  0.2989 * img[:, :, 0] + 0.5870 * img[:, :, 1] + 0.1140 * img[:, :, 2]
-    img_data.append(gray)
+    img = load_img(image_path)#Load image
+    img = img_to_array(img)#Convert image to array
+    img = cv.resize(img, (227,227), interpolation=cv.INTER_CUBIC)#Resize image to 227x227. Resizing too 227x227 because the it is perfectly divisible by 2 multiple times which is required for Conv3D layers.
+    gray =  0.2989 * img[:, :, 0] + 0.5870 * img[:, :, 1] + 0.1140 * img[:, :, 2]#Convert image to grayscale using luminosity method.
+    img_data.append(gray)#Append the processed image to img_data list.
 
-
+# Loop through all the training videos, extract frames from each video and store them temporarily in train_img_path. Then, load each frame, process it using data_store function and append it to img_data list.
 for video in train_videos:
+    #Extract frames from video and store them temporarily in train_img_path
     video_path = os.path.join(train_path, video)
-    cap = cv.VideoCapture(video_path)
-    isTrue, frame = cap.read()
-    count = 0
+    cap = cv.VideoCapture(video_path)  #Capture the video
+    isTrue, frame = cap.read()#Read the first frame
+    count = 0#Frame counter
 
+    #Loop to read each frame from the video
     while isTrue:
+        # Save the frame to the temporary folder
         frame_path = os.path.join(train_img_path, f"{count:03d}.jpg")
-        cv.imwrite(frame_path, frame)
-        isTrue, frame = cap.read()
-        count += 1
+        cv.imwrite(frame_path, frame)#Write the frame to the specified path
+        isTrue, frame = cap.read()#Read the next frame
+        count += 1#Increment frame counter
 
-    cap.release()
+    cap.release()#Release the video capture object
 
-    images = os.listdir(train_img_path)
+    images = os.listdir(train_img_path)#Get list of all frames stored in the temporary folder
 
+    #Process each frame using data_store function and append it to img_data list
     for img in images:
         image_path = os.path.join(train_img_path, img)
         data_store(image_path)
 
+#Convert img_data list to numpy array and reshape it to (num_frames, height, width)
 image_data = np.array(img_data)
 a,b,c = image_data.shape
-image_data = image_data.reshape(b,c,a)
-image_data = (image_data - image_data.mean()) / (image_data.std())
-image_data = np.clip(image_data, 0 , 1)
+image_data = image_data.reshape(b,c,a)#Reshape to (num_frames, height, width) because height is b and width is c. This is done to make it compatible for training.
+image_data = (image_data - image_data.mean()) / (image_data.std())#Normalize the pixel values
+image_data = np.clip(image_data, 0 , 1)#Clip the pixel values to be in the range [0, 1]
 
-np.save('./Avenue Dataset/training_data.npy', image_data)
+np.save('./Avenue Dataset/training_data.npy', image_data)#Save the processed data as a numpy array for later use in training
